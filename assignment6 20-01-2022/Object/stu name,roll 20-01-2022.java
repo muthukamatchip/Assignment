e9fb9da329c6722abb4dfda798d0814a5b8821a4
muthu <95739546+muthukamatchip@users.noreply.github.com>
2022-01-22 15:00:29 +0530

    Add files via upload

---
+++ assignment6 20-01-2022/Object/stu name,roll 20-01-2022.java
@@ -0,0 +1,16 @@
+package javaclass;
+
+public class object {
+		String name;
+		int rollno;
+
+
+	public static void main(String[] args) {
+		
+	object stu=new object();
+	stu.name="john";
+	stu.rollno=2;
+	System.out.println("Name of the Student is: "+stu.name);
+	System.out.println("Roll No is :"+stu.rollno);
+	}
+}
